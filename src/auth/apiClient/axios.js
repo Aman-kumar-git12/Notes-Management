b1d0cce7bb9b909b48b32c9e5b16823c93f6341c
Aman kumar <rajaman5562@gmail.com>
2025-11-25 13:21:53 +0530

    fixess

--- src/auth/apiClient/axios.js
+++ src/auth/apiClient/axios.js
@@ -14,4 +14,13 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error Response:", error.response?.data);
+    console.error("API Error Status:", error.response?.status);
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
